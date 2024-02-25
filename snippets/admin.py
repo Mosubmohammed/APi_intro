1cd4159d80523a88857ed96d55a1d5a081cf7ff7
Mosubmohammed <mosup.050@gmail.com>
2024-02-25 14:18:35 +0300

    .

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.register(Employyes)
+admin.site.register(Diabetes)
+admin.site.register(Clothes)
--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Employyes(models.Model):
+    name=models.CharField(max_length=50)
+    checkIn=models.DateTimeField(auto_now_add=True)
+    checkOut=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+    
+
+class Diabetes(models.Model):
+    glocos=models.FloatField(max_length=10)
+    bmi=models.FloatField(max_length=10)
+    prusser=models.FloatField(max_length=10)
+    
+    def __str__(self):
+        return self.glocos
+    
+
+class Clothes(models.Model):
+    name=models.CharField(max_length=50)
+    price=models.FloatField(max_length=10)
+    description=models.TextField()
+    
+    
+    def __str__(self):
+        return self.name
+    
